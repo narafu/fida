aa322618dc7900a4be5b1a66a9524d959763c2ba
흥수 박 <phs0918@kyobobook.com>
2026-05-20 19:38:20 +0900

    feat: NotifyPort에 notifyKistaSuccess/notifyKistaFailure 메서드 추가

--- src/main/java/com/fida/domain/port/out/NotifyPort.java
+++ src/main/java/com/fida/domain/port/out/NotifyPort.java
@@ -4,4 +4,8 @@ import com.fida.domain.model.TradingRecord;
 
 public interface NotifyPort {
     void notify(TradingRecord record);
+
+    void notifyKistaSuccess(TradingRecord record);
+
+    void notifyKistaFailure(TradingRecord record, Throwable cause);
 }
